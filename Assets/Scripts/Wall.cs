ddc6366859d1b3f1f1de82ad72e9a376eb0ef425
Lizi Attwood <lizi@beeski.com>
2015-03-08 14:27:55 +0000

    Unity Project showing 2D, SpriteRenderer and 2D physics

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour 
+{
+	[SerializeField] private SpriteRenderer BrickPrefab;
+	[SerializeField] private int NumPerRow;
+	[SerializeField] private int NumRows;
+
+	void Start() 
+	{
+		Build();
+	}
+
+	private void Build()
+	{
+		float w = BrickPrefab.sprite.bounds.size.x * BrickPrefab.transform.localScale.x;
+		float h = BrickPrefab.sprite.bounds.size.y * BrickPrefab.transform.localScale.y;
+		float start_x = ( ( NumPerRow ) * w ) * 0.5f;
+		float start_y = ( ( NumRows - 1 ) * h ) * 0.5f;
+		float x = 0.0f;
+		float y = -start_y;
+
+		for( int row = 0; row < NumRows; row++ )
+		{
+			x = -start_x + ( row % 2 == 0 ? 0 : w * 0.5f );
+			for( int column = 0; column < NumPerRow; column++ )
+			{
+				SpriteRenderer s = Instantiate( BrickPrefab ) as SpriteRenderer;
+				s.transform.position = new Vector3( x, y, 0.0f );
+				x += w;
+			}
+			y += h;
+		}
+	}
+}
